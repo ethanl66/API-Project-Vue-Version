33f2eb39b34063397a3c165d96d6067e789ce902
Ehan <ethanliang405@gmail.com>
2021-09-12 16:35:52 -0400

    navbar initial appearance

--- apiprojectvue/src/components/Navbar.vue
+++ apiprojectvue/src/components/Navbar.vue
@@ -1,33 +1,52 @@
 <template>
-  <nav class="navbar">
-    <a href="" class="navlink">Home</a>
-    <a href="" class="navlink">Anime</a>
-    <a href="" class="navlink">Manga</a>
-    <a href="" class="navlink">Light Novels</a>
-  </nav>
+  <div class="nav-container">
+    <div class="navbar">
+      <a href="" class="navlink">Home</a>
+      <a href="" class="navlink">Anime</a>
+      <a href="" class="navlink">Manga</a>
+      <a href="" class="navlink">Light Novels</a>
+      <span class="flex-grow-1"></span>
+      <search />
+    </div>
+  </div>
 </template>
 
 <script>
+import Search from "./Search.vue";
+
 export default {
   name: "Navbar",
+  components: { Search },
 };
 </script>
 
 <style lang="scss">
-.navbar {
-  background-color: var(--background-color);
+.nav-container {
+  //background-color: var(--background-color);
+  background-color: black;
   position: fixed;
   top: 0;
   z-index: 999;
   width: 100%;
-  padding: 2rem;
+  //padding: 2rem;
+}
+.navbar {
+  width: var(--section-width);
+  display: flex;
+  margin: auto;
+  align-items: center;
 }
 .navbar > a {
+  font-size: 1.8rem;
   color: var(--off-white);
   padding: 2rem;
 }
 .navbar > a:hover {
-  color: white;
+  color: rgb(19, 17, 17);
   background-color: var(--surface-color);
+}
+
+.flex-grow-1 {
+  flex-grow: 1;
 }
 </style>
